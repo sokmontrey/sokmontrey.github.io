896cc84fe0710b6360c034f8810e16fd0973226b
Sokmontrey <sythatsokmontrey@gmail.com>
2024-01-19 07:54:46 -0500

    move: separate Bio

---
+++ src/components/Bio.svelte
@@ -0,0 +1,12 @@
+<p class="font-mono text-base font-bold">
+  I'm Sokmontrey, a <span class="diff-text">math</span> enthusiast <br />
+  who found out about <span class="diff-text">computer</span> science.<br />
+  I <span class='diff-text'>learn, build,</span> and <span class='diff-text'>teach</span> stuffs.
+</p>
+
+<style>
+  .diff-text {
+    @apply font-serif text-xl;
+    color: var(--pm-color);
+  }
+</style>
