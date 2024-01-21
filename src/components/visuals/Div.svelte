5c9ea3fac0d424f5d9e083bf7c0b28a7f0cacadf
Sokmontrey <sythatsokmontrey@gmail.com>
2024-01-21 11:56:39 -0500

    feat: sync w and h with its css value

--- src/components/visuals/Div.svelte
+++ src/components/visuals/Div.svelte
@@ -1,32 +1,38 @@
 <script>
-  //ts-check
+    //ts-check
 
-  /** @type {Object} style - Style object
-    * Use regular css properties name and values
-    * 
-    * @example
-    * 'background-color': 'red',
-    */
-  export let style = {};
+    /** @type {Object} style - Style object
+     * Use regular css properties name and values
+     *
+     * @example
+     * 'background-color': 'red',
+     */
+    export let style = {};
 
-  /** @type {number} w - Width */
-  export let w = null;
-  /** @type {number} h - Height */
-  export let h = null;
+    /** @type {number} w - Width */
+    export let w = null;
+    /** @type {number} h - Height */
+    export let h = null;
 
-  /** @type {string} unit - Unit to be used*/
-  export let unit = "px";
+    /** @type {string} unit - Unit to be used*/
+    export let unit = "px";
+
+    function getWidth(node) {
+        if (w === null) w = node.clientWidth;
+        if (h === null) h = node.clientHeight;
+    }
 </script>
 
 <div
-  style={Object.entries({
-    ...style,
-    width: w + unit,
-    height: h + unit,
-  }).reduce((acc, [k, v]) => {
-    return acc + `${k}: ${v};`;
-  }, "")}
-  {...$$restProps}
+    use:getWidth
+    style={Object.entries({
+        ...style,
+        width: w + unit,
+        height: h + unit,
+    }).reduce((acc, [k, v]) => {
+        return acc + `${k}: ${v};`;
+    }, "")}
+    {...$$restProps}
 >
-  <slot w={w / 100} h={h / 100} />
+    <slot w={w / 100} h={h / 100} />
 </div>
