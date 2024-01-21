45d148388e196b5d8cea8a9d11275c5c60fcea6f
Sokmontrey <sythatsokmontrey@gmail.com>
2024-01-21 11:51:55 -0500

    feat: finally got the interactive thumbnail

--- src/components/ArticlePost.svelte
+++ src/components/ArticlePost.svelte
@@ -1,18 +1,18 @@
-<script>
-    export let article;
+<script >
+    export let date;
+    export let title;
+    export let description;
 </script>
 
 <section class="flex flex-col ">
     <p class=" nt-color opacity-40 z-0 "> 
-        {article.frontmatter.date || "January 23, 2024"}
+        {date || "January 23, 2024"}
     </p>
     <h2 class=" pm-color text-[2.3rem] font-bold">
-        {article.frontmatter.title || "Untitled"}
+        {title || "Untitled"}
     </h2>
-    <div class="flex-1 w-[100%] h-[500px] pt-[80%] my-4 bg-ft-color">
-        <article.thumbnail />
-    </div>
+    <slot name="thumbnail"></slot>
     <p class=" nt-color text-lg opacity-70">
-        {article.frontmatter.description || "No description"}
+        {description || "No description"}
     </p>
 </section>
