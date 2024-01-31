30706d5818887039713f07d7ee531cf93d2d0078
Sokmontrey <sythatsokmontrey@gmail.com>
2024-01-31 13:37:03 -0500

    adjust: style

--- src/components/ArticleSectionBar.svelte
+++ src/components/ArticleSectionBar.svelte
@@ -1,56 +1,56 @@
 <script>
-    import { onMount } from "svelte";
+	import { onMount } from "svelte";
 
-    const threshold = 0.6;
-    let sections_el = [];
-    let sections = [];
-    let active_index = 0;
+	const threshold = 0.6;
+	let sections_el = [];
+	let sections = [];
+	let active_index = 0;
 
-    onMount(() => {
-        sections_el = Array.from(document.querySelectorAll("h1")).map(
-            (el, i) => {
-                el.id = `section-${i}`;
-                return el;
-            },
-        );
-        sections = Array.from(sections_el).map((el) => el.innerText);
+	onMount(() => {
+		sections_el = Array.from(document.querySelectorAll("h1"))
+			.map((el, i) => {
+				el.id += `section-${i}`;
+				return el;
+			});
 
-        function getActiveSectionIndex() {
-            for (let i = sections.length - 1; i >= 0; i--) {
-                const el = sections_el[i];
-                const rect = el.getBoundingClientRect();
-                if (rect.y / window.innerHeight < threshold) {
-                    if (active_index !== i) active_index = i;
-                    break;
-                }
-            }
-        }
+		sections = Array.from(sections_el).map((el) => el.innerText);
 
-        window.addEventListener("scroll", () => getActiveSectionIndex());
-    });
+		function getActiveSectionIndex() {
+			for (let i = sections.length - 1; i >= 0; i--) {
+				const el = sections_el[i];
+				const rect = el.getBoundingClientRect();
+				if (rect.y / window.innerHeight < threshold) {
+					if (active_index !== i) active_index = i;
+					break;
+				}
+			}
+		}
+
+		window.addEventListener("scroll", () => getActiveSectionIndex());
+	});
 </script>
 
 <aside
-    class="hidden lg:flex flex-col justify-end fixed bottom-8 left-8 h-[100vh] font-mono"
+	class="hidden w-[100px] lg:flex flex-col justify-end fixed bottom-8 left-8 h-[100vh] font-mono"
 >
-    <ul class="">
-        {#each sections as section, i}
-            <li
-                class={`text-sm opacity-${
-                    i === active_index ? "100" : "50"
-                } hover:opacity-100 transition-opacity`}
-            >
-                <button
-                    class="cursor-pointer"
-                    on:click={() =>
-                        sections_el[i].scrollIntoView({
-                            behavior: "smooth",
-                            block: "center",
-                        })}
-                >
-                    {section}
-                </button>
-            </li>
-        {/each}
-    </ul>
+	<ul class="">
+		{#each sections as section, i}
+			<li
+				class={`text-sm opacity-${
+					i === active_index ? "100" : "50"
+				} hover:opacity-100 transition-opacity`}
+			>
+				<button
+					class="cursor-pointer text-left py-1 font-mono"
+					on:click={() =>
+						sections_el[i].scrollIntoView({
+							behavior: "smooth",
+							block: "center",
+						})}
+				>
+					{section}
+				</button>
+			</li>
+		{/each}
+	</ul>
 </aside>
