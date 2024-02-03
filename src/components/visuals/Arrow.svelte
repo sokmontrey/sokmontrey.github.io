b49b217adbf8ad5c659c9d3609ad452c91c61f24
Sokmontrey <sythatsokmontrey@gmail.com>
2024-02-02 22:42:58 -0500

    use awesome font icon for arrow tip

--- src/components/visuals/Arrow.svelte
+++ src/components/visuals/Arrow.svelte
@@ -1,34 +1,38 @@
 <script>
 	export let stroke_size = 2;
 	export let length = 50;
-	export let tip_length = 10;
-	export let color = 'var(--nt-color)';
+	export let color = "var(--nt-color)";
+	export let dir = "down";
+
+	let tip_style = `
+	color: ${color};
+	`;
 </script>
 
-<div {...$$restProps}>
+<div
+	{...$$restProps}
+	class="flex justify-center items-center"
+	style={`
+		flex-direction: ${dir === "down" || dir === "up" ? "column" : "row"};
+	`}
+>
+	{#if dir === "up"}
+		<i class="fa-solid fa-chevron-up translate-y-3 " style={tip_style}></i>
+	{:else if dir === "left"}
+		<i class="fa-solid fa-chevron-left translate-y-2 " style={tip_style}></i>
+	{/if}
+
 	<div
 		style={`
-			width: ${stroke_size}px;
-			height: ${length}px;
-			background-color: ${color};
+		width: ${dir === "down" || dir === "up" ? stroke_size : length}px;
+		height: ${dir === "down" || dir === "up" ? length : stroke_size}px;
+		background-color: ${color};
 		`}
-	></div>
-	<div
-		style={`
-			width: ${stroke_size}px;
-			height: ${tip_length}px;
-			transform: translate(${tip_length /3}px, -${tip_length / 1.3}px) rotate(45deg) ;
-			background-color: ${color};
-		`}
-		class="absolute"
-	></div>
-	<div
-		style={`
-			width: ${stroke_size}px;
-			height: ${tip_length}px;
-			transform: translate(-${tip_length /3}px, -${tip_length / 1.3}px) rotate(-45deg) ;
-			background-color: ${color};
-		`}
-		class="absolute"
-	></div>
+	/>
+
+	{#if dir === "down"}
+		<i class="fa-solid fa-chevron-down -translate-y-3 " style={tip_style}></i>
+	{:else if dir === "right"}
+		<i class="fa-solid fa-chevron-right -translate-x-2 " style={tip_style}></i>
+	{/if}
 </div>
