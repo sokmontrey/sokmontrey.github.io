459313b28d7353ed92280b32c77222f993a5c96c
Sokmontrey <sythatsokmontrey@gmail.com>
2024-02-05 12:09:36 -0500

    Comp graph intro & StraightGraph

--- src/components/visuals/Circle.svelte
+++ src/components/visuals/Circle.svelte
@@ -1,26 +1,16 @@
 <script>
-	import Rect from "@/components/visuals/Rect.svelte";
-
-	/** @type {Object} style - Style object
-	 * Use regular css properties name and values
-	 *
-	 * @example
-	 * 'background-color': 'red',
-	 */
-	export let style = {};
-
-	/** @type {number} r */
-	export let r = null;
+	export let r = "100px";
+	export let fill = "var(--nt-color)";
 </script>
 
-<Rect
-	style={{
-		...style,
-		"border-radius": "100%",
-		width: style["width"] || `calc(2 * ${r})`,
-		height: style["height"] || `calc(2 * ${r})`,
-	}}
+<div
 	{...$$restProps}
+	style={`
+		width: ${r};
+		height: ${r};
+		border-radius: 100%;
+		background-color: ${fill};
+	`}
 >
-	<slot w={r * 2} h={r * 2} />
-</Rect>
+	<slot />
+</div>
---
+++ src/pages/articles/computational_graph/StraightGraph.svelte
@@ -0,0 +1,42 @@
+<script>
+	import Circle from "@/components/visuals/Circle.svelte";
+	import Arrow from "@/components/visuals/Arrow.svelte";
+	import Katex from "svelte-katex";
+	import { interpolate } from "d3-interpolate";
+
+	const px = "px";
+
+	let w;
+	$: r = w * 0.08;
+
+	function color(t) {
+		return interpolate("#85ff9e", "#1a58d3")(t);
+	}
+</script>
+
+<div
+	bind:clientWidth={w}
+	class="w-full h-full flex justify-center items-center my-10 "
+>
+	<Circle r={r + px} fill={color(0)} class="flex justify-center items-center bg-color ">
+		<Katex>x</Katex>
+	</Circle>
+	<Arrow dir="right" class="pl-3" />
+	<Circle r={r + px} fill={color(0.3)} class="flex justify-center items-center bg-color ">
+		<Katex>\times 5</Katex>
+	</Circle>
+	<Arrow dir="right" class="pl-3" />
+	<Circle r={r + px} fill={color(0.6)} class="flex justify-center items-center bg-color "
+	>
+		<Katex>+ 3</Katex>
+	</Circle>
+	<Arrow dir="right" class="pl-3" />
+	<Circle r={r + px} fill={color(1)} class="flex justify-center items-center nt-color "
+	>
+		<Katex>e^x</Katex>
+	</Circle>
+	<Arrow dir="right" class="pl-3" />
+	<Circle r={r + px} fill="" class="flex justify-center items-center nt-color ">
+		<Katex>f</Katex>
+	</Circle>
+</div>
