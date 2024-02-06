891ee2b61816804c1b2d580e9056b383dcd4c0ac
Sokmontrey <sythatsokmontrey@gmail.com>
2024-02-05 22:45:50 -0500

    done: comp graph, stated: auto diff

---
+++ src/pages/articles/computational_graph/NotStraightGraph.svelte
@@ -0,0 +1,66 @@
+<script>
+  import Katex from "svelte-katex";
+  import Circle from "@/components/visuals/Circle.svelte";
+  import Arrow from "@/components/visuals/Arrow.svelte";
+
+  const px = "px";
+
+  let w;
+  $: r = w * 0.08;
+
+  const circle_class = "flex justify-center items-center nt-color bg-blue-500/50 ";
+</script>
+
+<div
+  bind:clientWidth={w}
+  class="w-full h-full flex justify-center items-center my-10"
+>
+  <Circle
+    r={r + px}
+    fill=""
+    class={circle_class}
+  >
+    <Katex>x</Katex>
+  </Circle>
+  <Arrow dir="right" class="pl-2 " />
+  <Circle
+    r={r + px}
+    fill=""
+    class="flex justify-center items-center nt-color bg-orange-500/50 "
+  >
+    <Katex>sin</Katex>
+  </Circle>
+  <div class=" flex flex-col justify-center items-center ml-2">
+    <Arrow dir="right" class=" -rotate-[30deg] origin-left " />
+    <Arrow dir="right" class=" rotate-[30deg] origin-left " />
+  </div>
+  <div class=" flex flex-col justify-center items-center">
+    <Circle
+      r={r + px}
+      fill=""
+      class={circle_class}
+    >
+      <Katex>x^2</Katex>
+    </Circle>
+    <Circle
+      r={r + px}
+      fill=""
+      class={circle_class + " mt-5 "}
+    >
+      <Katex>\times 3</Katex>
+    </Circle>
+  </div>
+  <div class=" flex flex-col justify-center items-center ml-2">
+    <Arrow dir="right" class=" rotate-[30deg] origin-right " />
+    <Arrow dir="right" class=" -rotate-[30deg] origin-right " />
+  </div>
+  <Circle
+    r={r + px}
+    fill=""
+    class={circle_class}
+  >
+    <Katex>+</Katex>
+  </Circle>
+  <Arrow dir="right" class="pl-2" />
+  <Katex> g </Katex>
+</div>
--- src/pages/articles/computational_graph/StraightGraph.svelte
+++ src/pages/articles/computational_graph/StraightGraph.svelte
@@ -1,42 +1,51 @@
 <script>
-	import Circle from "@/components/visuals/Circle.svelte";
-	import Arrow from "@/components/visuals/Arrow.svelte";
-	import Katex from "svelte-katex";
-	import { interpolate } from "d3-interpolate";
+  import Circle from "@/components/visuals/Circle.svelte";
+  import Arrow from "@/components/visuals/Arrow.svelte";
+  import Katex from "svelte-katex";
 
-	const px = "px";
+  const px = "px";
 
-	let w;
-	$: r = w * 0.08;
+  let w;
+  $: r = w * 0.08;
 
-	function color(t) {
-		return interpolate("#85ff9e", "#1a58d3")(t);
-	}
+  const circle_class = "flex justify-center items-center nt-color bg-blue-500/50 ";
 </script>
 
 <div
-	bind:clientWidth={w}
-	class="w-full h-full flex justify-center items-center my-10 "
+  bind:clientWidth={w}
+  class="w-full h-full flex justify-center items-center my-10"
 >
-	<Circle r={r + px} fill={color(0)} class="flex justify-center items-center bg-color ">
-		<Katex>x</Katex>
-	</Circle>
-	<Arrow dir="right" class="pl-3" />
-	<Circle r={r + px} fill={color(0.3)} class="flex justify-center items-center bg-color ">
-		<Katex>\times 5</Katex>
-	</Circle>
-	<Arrow dir="right" class="pl-3" />
-	<Circle r={r + px} fill={color(0.6)} class="flex justify-center items-center bg-color "
-	>
-		<Katex>+ 3</Katex>
-	</Circle>
-	<Arrow dir="right" class="pl-3" />
-	<Circle r={r + px} fill={color(1)} class="flex justify-center items-center nt-color "
-	>
-		<Katex>e^x</Katex>
-	</Circle>
-	<Arrow dir="right" class="pl-3" />
-	<Circle r={r + px} fill="" class="flex justify-center items-center nt-color ">
-		<Katex>f</Katex>
-	</Circle>
+  <Circle
+    r={r + px}
+    fill=""
+    class={circle_class}
+  >
+    <Katex>x</Katex>
+  </Circle>
+  <Arrow dir="right" class="pl-3" />
+  <Circle
+    r={r + px}
+    fill=""
+    class={circle_class}
+  >
+    <Katex>\times 5</Katex>
+  </Circle>
+  <Arrow dir="right" class="pl-3" />
+  <Circle
+    r={r + px}
+    fill=""
+    class={circle_class}
+  >
+    <Katex>+ 3</Katex>
+  </Circle>
+  <Arrow dir="right" class="pl-3" />
+  <Circle
+    r={r + px}
+    fill=""
+    class={circle_class}
+  >
+    <Katex>e^x</Katex>
+  </Circle>
+  <Arrow dir="right" class="pl-3" />
+  <Katex>f</Katex>
 </div>
