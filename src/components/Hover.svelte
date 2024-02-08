55a4e4da4c9444ea89f3374d99ec4408102f75c3
Sokmontrey <sythatsokmontrey@gmail.com>
2024-02-08 11:23:01 -0500

    FEAT: Hover, ADJ: elementary,combinational rule

---
+++ src/components/Hover.svelte
@@ -0,0 +1,30 @@
+<span id="tooltip-text" {...$$restProps}>
+  <slot name="toggler" />
+</span>
+
+<div
+  id="tooltip"
+  class="absolute z-100 inline-block px-3 py-2 bg-bg-color nt-color transition-opacity duration-300 rounded-lg opacity-0 border border-[var(--ft-color)]"
+>
+  <slot />
+</div>
+
+<style>
+  #tooltip-text {
+    color: var(--sd-color);
+    cursor: pointer;
+    opacity: 1;
+    font-weight: bold;
+  }
+
+  #tooltip {
+    visibility: hidden;
+    box-shadow: 0px 0rem 2rem var(--bg-color);
+  }
+
+  #tooltip-text:hover + #tooltip,
+  #tooltip:hover {
+    visibility: visible;
+    opacity: 1;
+  }
+</style>
