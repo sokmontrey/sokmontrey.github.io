d03e22b4334e4100dfb3ee4cf89fa6dd60e3fc5c
Sokmontrey <sythatsokmontrey@gmail.com>
2024-02-08 20:55:45 -0500

    feat: Hover

--- src/components/Hover.svelte
+++ src/components/Hover.svelte
@@ -1,30 +1,11 @@
-<span id="tooltip-text" {...$$restProps}>
-  <slot name="toggler" />
+<span class="relative">
+  <span class="tooltip-text">
+    <slot name="toggler" />
+  </span>
+
+  <span
+    class="tooltip float-left absolute inline px-3 py-2 bg-bg-color nt-color transition-opacity duration-300 rounded-lg opacity-0 border border-[var(--ft-color)]"
+  >
+    <slot />
+  </span>
 </span>
-
-<div
-  id="tooltip"
-  class="absolute z-100 inline-block px-3 py-2 bg-bg-color nt-color transition-opacity duration-300 rounded-lg opacity-0 border border-[var(--ft-color)]"
->
-  <slot />
-</div>
-
-<style>
-  #tooltip-text {
-    color: var(--sd-color);
-    cursor: pointer;
-    opacity: 1;
-    font-weight: bold;
-  }
-
-  #tooltip {
-    visibility: hidden;
-    box-shadow: 0px 0rem 2rem var(--bg-color);
-  }
-
-  #tooltip-text:hover + #tooltip,
-  #tooltip:hover {
-    visibility: visible;
-    opacity: 1;
-  }
-</style>
