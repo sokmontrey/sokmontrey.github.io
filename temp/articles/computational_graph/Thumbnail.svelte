05d07bb4829eeae759af1ab1aff4068db7456f4e
Sokmontrey <sythatsokmontrey@gmail.com>
2024-06-04 18:10:07 -0400

    clean up old article files

--- temp/articles/computational_graph/Thumbnail.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Div from "@/components/visuals/Div.svelte";
-	// import signal from "@/utils/signal";
-	// import { onMount } from "svelte";
-	// import { cubicInOut } from "svelte/easing";
-	// import { interpolate } from "flubber";
-</script>
-
-<Div let:w class="w-full h-full"></Div>
