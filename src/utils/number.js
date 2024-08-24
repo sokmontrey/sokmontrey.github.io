715cd026d4f3bf400dd85248cfc82e39486f77bf
Sokmontrey <sythatsokmontrey@gmail.com>
2024-08-24 14:22:35 -0400

    table of contents

---
+++ src/utils/number.js
@@ -0,0 +1,18 @@
+export function toRoman(num) {
+  const roman_values = {
+    x: 10,
+    ix: 9,
+    v: 5,
+    iv: 4,
+    i: 1,
+  };
+
+  let roman = "";
+  for (let key in roman_values) {
+    while (num >= roman_values[key]) {
+      roman += key;
+      num -= roman_values[key];
+    }
+  }
+  return roman;
+}
