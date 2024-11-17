0f3a4174a577606aa4c860ce505dc3c031f391d7
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-17 15:16:55 -0500

    declare content config

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection } from 'astro:content';
+
+const projects = defineCollection({ 
+
+});
+
+const experiences = defineCollection({ 
+
+});
+
+const extra_curriculars = defineCollection({ 
+
+});
+
+export const collections = {
+	projects, experiences, extra_curriculars
+};
