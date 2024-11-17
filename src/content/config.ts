935889ea5ac50a5e4193be333dc2f0016fc8c820
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-17 15:27:34 -0500

    use one common schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,36 +1,27 @@
 import { defineCollection, z } from 'astro:content';
 
+const common_schema = z.object({
+	title: z.string(),
+	type: z.array(z.string()),
+	tags: z.array(z.string()),
+	description: z.string(),
+	url: z.string().optional(),
+	image: z.array(z.string().optional()),
+})
+
 const projects = defineCollection({ 
 	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		type: z.array(z.string()),
-		tags: z.array(z.string()),
-		description: z.string(),
-		url: z.string().optional(),
-	}),
+	schema: common_schema,
 });
 
 const experiences = defineCollection({ 
 	type: 'data',
-	schema: z.object({
-		title: z.string(),
-		type: z.array(z.string()),
-		tags: z.array(z.string()),
-		description: z.string(),
-		url: z.string().optional(),
-	}),
+	schema: common_schema,
 });
 
 const extra_curriculars = defineCollection({ 
 	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		type: z.array(z.string()),
-		tags: z.array(z.string()),
-		description: z.string(),
-		url: z.string().optional(),
-	}),
+	schema: common_schema,
 });
 
 export const collections = {
