1e30a62eb1d08f72eea2eca0ec460f3959e9326f
Sokmontrey <sythatsokmontrey@gmail.com>
2024-11-17 15:25:07 -0500

    config all

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,36 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 
 const projects = defineCollection({ 
-
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		type: z.array(z.string()),
+		tags: z.array(z.string()),
+		description: z.string(),
+		url: z.string().optional(),
+	}),
 });
 
 const experiences = defineCollection({ 
-
+	type: 'data',
+	schema: z.object({
+		title: z.string(),
+		type: z.array(z.string()),
+		tags: z.array(z.string()),
+		description: z.string(),
+		url: z.string().optional(),
+	}),
 });
 
 const extra_curriculars = defineCollection({ 
-
+	type: 'content',
+	schema: z.object({
+		title: z.string(),
+		type: z.array(z.string()),
+		tags: z.array(z.string()),
+		description: z.string(),
+		url: z.string().optional(),
+	}),
 });
 
 export const collections = {
