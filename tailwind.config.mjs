6383b9cbf68482fb2160e3cccc8a8f1c2dfa6d5a
Sokmontrey <sythatsokmontrey@gmail.com>
2025-12-27 11:43:28 -0500

    feat: add custom fonts and integrate them with Tailwind CSS.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				serif: ['EB Garamond', 'serif'],
+				sans: ['Poppins', 'sans-serif'],
+				mono: ['DM Mono', 'monospace'],
+			},
+		},
 	},
 	plugins: [],
 };
